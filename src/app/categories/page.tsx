bce2bcf1a7335f15dbf673eaba2ebc41ff235015
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2025-05-02 09:53:19 +0200

    categories page desktop

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,21 @@
+import { ProductCard } from "../components/product-card/product-card";
+import styles from "./page.module.css";
+
+export default function CategoriesPage() {
+  return (
+    <div className={styles["categories-page"]}>
+      <h1 className={styles["categories-page__title"]}>Abbigliamento</h1>
+
+      <div className={styles["categories-page__products"]}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/product-card/product-card.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "./product-card.module.css";
+
+export function ProductCard() {
+  return (
+    <div className={styles["product-card"]}>
+      <img
+        src="https://it.slamjam.com/cdn/shop/files/NewBalance-Footwear-Low-U990gr4Grey-U990GR4-20250203110752_01.jpg"
+        alt="New Balance Made in USA 990v4"
+        className={styles["product-card__image"]}
+      />
+
+      <div className={styles["product-card__information"]}>
+        <p className={styles["product-card__brand"]}>New Balance</p>
+        <p className={styles["product-card__name"]}>Made in USA 990v4</p>
+        <p className={styles["product-card__price"]}>240 EUR</p>
+      </div>
+    </div>
+  );
+}
