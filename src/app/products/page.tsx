426d2cdf86d5367c25a7006c4aac3092fd705512
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2025-05-03 09:17:13 +0200

    refactor product page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,28 +6,30 @@ import styles from "./page.module.scss";
 export default function ProductPage() {
   return (
     <div className={styles["product-page"]}>
-      <div className={styles["product-page__carousel"]}>
+      <div className={styles["product-page__media"]}>
         <img
           src="https://it.slamjam.com/cdn/shop/files/NewBalance-Footwear-Low-U990gr4Grey-U990GR4-20250203110752_01.jpg"
           alt="New Balance Made in USA 990v4"
-          className={styles["product-page__carousel__image"]}
+          className={styles["product-page__media__image"]}
         />
 
         <img
           src="https://it.slamjam.com/cdn/shop/files/NewBalance-Footwear-Low-U990gr4Grey-U990GR4-20250203110752_01.jpg"
           alt="New Balance Made in USA 990v4"
-          className={styles["product-page__carousel__image"]}
+          className={styles["product-page__media__image"]}
         />
       </div>
 
-      <div className={styles["product-page__information"]}>
-        <p className={styles["product-page__information__brand"]}>
-          New Balance
-        </p>
-        <p className={styles["product-page__information__name"]}>
-          Made in USA 990v4
-        </p>
-        <p className={styles["product-page__information__price"]}>240 EUR</p>
+      <div>
+        <div className={styles["product-page__information"]}>
+          <p className={styles["product-page__information__brand"]}>
+            New Balance
+          </p>
+          <p className={styles["product-page__information__name"]}>
+            Made in USA 990v4
+          </p>
+          <p className={styles["product-page__information__price"]}>240 EUR</p>
+        </div>
 
         <form className={styles["product-page__buy-area"]}>
           <fieldset className={styles["product-page__buy-area__size-selector"]}>
