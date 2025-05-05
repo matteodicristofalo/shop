a56228f046929e93fa34aa039564d073dc0220ec
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2025-05-05 21:07:29 +0200

    no longer get cart totalQuantity from shopify

--- src/app/api/add-to-cart/route.ts
+++ src/app/api/add-to-cart/route.ts
@@ -40,7 +40,6 @@ export async function POST(request: Request) {
               quantity
             }
           }
-          totalQuantity
           checkoutUrl
         }
       }
--- src/app/api/get-cart/route.ts
+++ src/app/api/get-cart/route.ts
@@ -34,7 +34,6 @@ export async function POST(request: Request) {
           quantity
         }
       }
-      totalQuantity
       checkoutUrl
     }
   }`;
