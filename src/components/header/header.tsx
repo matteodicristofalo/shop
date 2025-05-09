2d5fa3d669aa193201ca055f60d415ac0f2a948f
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2025-05-09 22:45:33 +0200

    center header logo on desktop

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,14 @@
 import { CartButton } from "@components/ctas/cart-button";
 import { MenuButton } from "@components/ctas/menu-button";
+import Link from "next/link";
 import styles from "./header.module.scss";
 
 export function Header() {
   return (
     <header className={styles["header"]}>
-      <h1 className={styles["header__logo"]}>Slam Jam</h1>
+      <h1 className={styles["header__logo"]}>
+        <Link href="/">Slam Jam</Link>
+      </h1>
 
       <ul className={styles["header__ctas"]}>
         <li>
