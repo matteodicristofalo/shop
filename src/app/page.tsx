e5583319604ce1f1d5098f4e0c56a5485b9f7a49
Matteo Di Cristofalo <dicristofalomatteo@gmail.com>
2025-05-15 22:43:45 +0200

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
+import styles from "./page.module.scss";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles["home-page"]}>
+      <img
+        src="/cover.jpg"
+        alt="New in cover"
+        className={styles["home-page__new-in-cover"]}
+      />
+
+      <Link
+        href="/collections/new-in"
+        className={styles["home-page__new-in-cta"]}
+      >
+        New in
+      </Link>
+    </div>
+  );
 }
